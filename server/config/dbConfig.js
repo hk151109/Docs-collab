8ecebf2bcb590419ddf22f2f500f0dbcdfc2308c
Harikrishnan Gopal <118685394+hk151109@users.noreply.github.com>
2025-03-07 16:22:36 +0530

    deploy commit2

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(mongodb+srv://harikrishnang:4NJa7SpFhhHo9P2F@cluster0.ekb6c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
         console.log("MongoDB connection SUCCESS");
     } catch (error) {
         console.error("MongoDB connection FAIL");
